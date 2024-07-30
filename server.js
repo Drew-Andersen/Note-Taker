4a5655e840556bfce9ddf54cba2350f8432856ab
Drew-Andersen <andrewtandersen@gmail.com>
2024-07-30 18:46:52 -0400

    Added the server.js file

---
+++ server.js
@@ -0,0 +1,106 @@
+// import express
+const { info } = require('console');
+const express = require('express');
+// import file system module
+const fs = require('fs');
+// import path
+const path = require('path');
+
+// Creates new app with express
+const app = express();
+// Port
+const port = 3001;
+
+// Middleware for parsing JSOJN and urlencoding form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// Get route which sends back the index.html page
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+// Get route which sends back the notes.html page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+})
+
+// Get route to read the db.json file and send back the parse JSON data
+app.get('/app/notes', (req, res) => {
+    fs.readFile('db/db.json', 'utf8', (err, res) => {
+        let jsonData = JSON.parse(data);
+        console.log(jsonData);
+        res.json(jsonData);
+    })
+})
+
+// Reads newly added notes from the request body and then adds them to the db.json file
+const readThenAppendToJSON = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parseData = JSON.parse(data);
+            parseData.push(content);
+            writeNewNoteToJson(file, parseData);
+        }
+    })
+}
+
+// Writes data to db.json 
+// utilized within the readThenAppendToJson function
+const writeNewNoteToJson = (destination, content) => {
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(`Destination written to ${destination}`);
+        }
+    })
+}
+
+// Post
+app.post('/api/notes', (req,res) => {
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title: title,
+            text: text,
+            id: uniqid()
+        }
+
+        readThenAppendToJSON(newNote, 'db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote
+        }
+
+        res.json(responsel);
+    } else {
+        res.json('Error in posting new note');
+    }
+})
+
+// Delete
+app.delete("/api/notes/:id", (req, res) => {
+    let id = req.params.id;
+    let parsedData;
+    fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        parsedData = JSON.parse(data);
+        const filterData = parsedData.filter((note) => note.id !== id);
+        writeNewNoteToJson("Develop/db/db.json", filterData);
+      }
+    });
+    res.send(`Deleted note with ${req.params.id}`);
+  });
+  
+  // App.listen is used to spin up our local server
+  app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+  );
